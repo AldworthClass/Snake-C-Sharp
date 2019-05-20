816580f2475b350295fbcd146fcd69cc0664bedd
AldworthClass <46466802+AldworthClass@users.noreply.github.com>
2019-05-20 15:08:39 -0400

    Added snake class, and methods. Does not repaint properly.

---
+++ Snake C Sharp/Snake.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_C_Sharp
+{
+	
+	class Snake
+	{
+		private Graphics snakeWindow;	//Drawing surface
+		private List<Point> snake;		//Snake tiles
+		private int width;				//width of snake tile
+		private int height;				//height of snake tile
+		private Size boardSize;			//board size
+		private String direction;
+
+		public Snake(Graphics board, Point start, Size boardSize, Size snakeSize)
+		{
+			direction = "u";
+			snake = new List<Point>();
+			this.boardSize = boardSize;
+			width = snakeSize.Width;
+			height = snakeSize.Height;
+			snakeWindow = board;
+			snake.Add(start);
+		}
+		public void grow()
+		{
+			if (direction == "u")
+				snake.Add(new Point(snake[snake.Count - 1].X, snake[snake.Count - 1].Y + 1));
+			else if (direction == "d")
+				snake.Add(new Point(snake[snake.Count - 1].X, snake[snake.Count - 1].Y - 1));
+			else if (direction == "l")
+				snake.Add(new Point(snake[snake.Count - 1].X + 1, snake[snake.Count - 1].Y));
+			else
+				snake.Add(new Point(snake[snake.Count - 1].X - 1, snake[snake.Count - 1].Y));
+		}
+		public void move()
+		{
+			//Starts with last item
+			for(int i = snake.Count - 1; i >=0; i--)
+			{
+				if (i > 0)
+					snake[i] = snake[i - 1];
+				else
+					if (direction == "u")
+						snake[0] = (new Point(snake[0].X, snake[0].Y - 1));
+					else if (direction == "d")
+						snake[0]=(new Point(snake[0].X, snake[0].Y + 1));
+					else if (direction == "l")
+						snake[0]=(new Point(snake[0].X - 1, snake[0].Y));
+					else
+						snake[0]=(new Point(snake[0].X + 1, snake[0].Y));
+
+			}
+
+		}
+
+		public void draw()
+		{
+			SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Green);
+			for (int i = 0; i < snake.Count; i++)
+				snakeWindow.FillEllipse(myBrush, new Rectangle(snake[i].X * width, snake[i].Y * height, width, height));
+		}
+
+		public void directionUp()
+		{
+			direction = "u";
+		}
+		public void directionDown()
+		{
+			direction = "d";
+		}
+		public void directionLeft()
+		{
+			direction = "l";
+		}
+		public void directionRight()
+		{
+			direction = "r";
+		}
+	}
+}
--- Snake C Sharp/SnakeGame.cs
+++ Snake C Sharp/SnakeGame.cs
@@ -12,6 +12,9 @@ namespace Snake_C_Sharp
 {
 	public partial class SnakeGame : Form
 	{
+		Graphics snakeWindow;
+		Snake snake;
+
 		public SnakeGame()
 		{
 			InitializeComponent();
@@ -20,7 +23,36 @@ namespace Snake_C_Sharp
 		private void SnakeGame_Load(object sender, EventArgs e)
 		{
 			this.ClientSize = new Size(600, 600);
+			snakeWindow = this.CreateGraphics();
+			snake = new Snake(snakeWindow, new Point(20, 20), new Size(40, 40), new Size(15, 15));
+
 			
+		}
+
+		private void SnakeGame_Paint(object sender, PaintEventArgs e)
+		{
+			
+			Rectangle myRectangle;
+			SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+			
+			//Paints the grid background
+			for (int i = 0; i < 40; i++)
+				for (int j = 0; j < 40; j++)
+				{
+					myRectangle = new Rectangle(i * 15, j * 15, 14, 14);
+					snakeWindow.FillRectangle(myBrush, myRectangle);
+				}
+			snake.draw();
+					
+			
+			//Pen myPen = new Pen(System.Drawing.Color.Red, 0);
+			
+		}
+
+		private void GameLoop_Tick(object sender, EventArgs e)
+		{
+			snake.move();
+			this.Refresh();
 		}
 	}
 }
